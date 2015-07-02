8df4adfe52838bddab7d592875f76d7a61ce5316
quanze <manqz@mcmaster.ca>
2015-07-02 09:40:03 -0400

    Attempt at making filter
    Not functional yet

--- InsultCtrl.js
+++ InsultCtrl.js
@@ -4,7 +4,7 @@ function InsultCtrl(titleCase) {
    this.derogatoryNouns = ['apple-john', 'baggage', 'barnacle', 'bladder', 'boar-pig', 'bugbear', 'bum-bailey', 'canker-blossom', 'clack-dish', 'clotpole', 'coxcomb', 'codpiece', 'death-token', 'dewberry', 'flap-dragon', 'flax-wench', 'flirt-gill', 'foot-licker', 'fustilarian', 'giglet', 'gudgeon', 'haggard', 'harpy', 'hedge-pig', 'horn-beast', 'hugger-mugger', 'jolthead', 'lewdster', 'lout', 'maggot-pie', 'malt-worm', 'mammet', 'measle', 'minnow', 'miscreant', 'moldwarp', 'mumble-news', 'nut-hook', 'pigeon-egg', 'pignut', 'puttock', 'pumpion', 'ratsbane', 'scut', 'skainsmate', 'strumpet', 'varlet', 'vassal', 'whey-face', 'wagtail'];
  	 this.titleCase = titleCase;
 
-   this.insult = titleCase.caps(this.generateInsult());
+   this.insult = this.generateInsult();
 }
 InsultCtrl.prototype.generateInsult = function() {
   var randomAdjective1 = this.primaryAdjectives[Math.floor(Math.random()*this.primaryAdjectives.length)];
--- caplitalizeFilter.js
+++ caplitalizeFilter.js
@@ -1,17 +1,14 @@
+function capitalize() {
+    return function(arr) {
+        var arr = insult.split("");
+        arr[0] = arr[0].toUpperCase();
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i] == " " || arr[i] == "-") {
+                arr[i + 1] = arr[i + 1].toUpperCase();
+            }
+        }
+        return arr.join('');
+    };
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-angular.module('insultApp').filter('capitalize',capitalize)
+angular.module('insultApp').filter('capitalize', capitalize)
